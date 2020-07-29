b222635ba62fee1b7cc66a629232e5cfa50f9aec
Tan Yen Khoon <tankhoon@gmail.com>
2020-07-29 14:12:28 +0800

    update

--- tests/env.ts
+++ tests/env.ts
@@ -87,103 +87,11 @@ const localnet: Node = {
 
 
 
-const uatnet: Node = {
-    connection: {
-        url: "https://uatnet.usdp.io",
-        timeout: 60000
-    },
-
-    trace: {
-        silent: true,
-        silentRpc: true
-    },
-
-    chainId: "uatnet",
-    name: "mxw",
-    airDrop: "roof voyage expire ball image despair soldier token destroy rocket couch drink",
-    kyc: {
-        provider: "into demand chief rubber raw hospital unit tennis sentence fade flight cluster",
-        issuer: "pill maple dutch predict bulk goddess nice left paper heart loan fresh",
-        middleware: "avocado trade bright wolf marble penalty mimic curve funny name certain visa"
-    },
-    alias: {
-        provider: "",
-        issuer: "",
-        middleware: "",
-        feeCollector: ""
-    },
-
-    fungibleToken: {
-        provider: "mother paddle fire dolphin nuclear giggle fatal crop cupboard close abandon truck",
-        issuer: "dynamic car culture shell kiwi harsh tilt boost vote reopen arrow moon",
-        middleware: "hospital item sad baby mass turn ability exhibit obtain include trip please",
-        feeCollector: "mxw1qgwzdxf66tp5mjpkpfe593nvsst7qzfxzqq73d"
-    },
-
-    nonFungibleToken: {
-        provider: "mother paddle fire dolphin nuclear giggle fatal crop cupboard close abandon truck",
-        issuer: "dynamic car culture shell kiwi harsh tilt boost vote reopen arrow moon",
-        middleware: "hospital item sad baby mass turn ability exhibit obtain include trip please",
-        feeCollector: "mxw1qgwzdxf66tp5mjpkpfe593nvsst7qzfxzqq73d"
-
-    }
-
-};
-
-
-const testnet: Node = {
-    connection: {
-        url: "https://alloys-rpc.maxonrow.com",
-        timeout: 60000
-    },
-
-    trace: {
-        silent: true,
-        silentRpc: true
-    },
-
-    chainId: "alloys",
-    name: "mxw",
-    airDrop: "roof voyage expire ball image despair soldier token destroy rocket couch drink",
-    kyc: {
-        provider: "into demand chief rubber raw hospital unit tennis sentence fade flight cluster",
-        issuer: "pill maple dutch predict bulk goddess nice left paper heart loan fresh",
-        middleware: "avocado trade bright wolf marble penalty mimic curve funny name certain visa"
-    },
-    alias: {
-        provider: "",
-        issuer: "",
-        middleware: "",
-        feeCollector: ""
-    },
-
-    fungibleToken: {
-        provider: "mother paddle fire dolphin nuclear giggle fatal crop cupboard close abandon truck",
-        issuer: "dynamic car culture shell kiwi harsh tilt boost vote reopen arrow moon",
-        middleware: "hospital item sad baby mass turn ability exhibit obtain include trip please",
-        feeCollector: "mxw1qgwzdxf66tp5mjpkpfe593nvsst7qzfxzqq73d"
-    },
-
-    nonFungibleToken: {
-        provider: "mother paddle fire dolphin nuclear giggle fatal crop cupboard close abandon truck",
-        issuer: "dynamic car culture shell kiwi harsh tilt boost vote reopen arrow moon",
-        middleware: "hospital item sad baby mass turn ability exhibit obtain include trip please",
-        feeCollector: "mxw1qgwzdxf66tp5mjpkpfe593nvsst7qzfxzqq73d"
-
-    }
-
-};
-
-
-
-const nodes: { [name: string]: Node } = { localnet, uatnet, testnet };
+const nodes: { [name: string]: Node } = { localnet };
  const nodeProvider: Node = localnet;
-// const nodeProvider: Node = devnet;
- // const nodeProvider: Node = uatnet;
- //const nodeProvider: Node = testnet;
 
 export {
-    nodeProvider, nodes, localnet, uatnet, testnet,
+    nodeProvider, nodes, localnet,
     Node
 };
 
